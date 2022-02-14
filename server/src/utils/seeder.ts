95626fa3e20c6526aeb971eed9394d7058fe5f7e
Murat KARACA <murat.github@hotmail.com>
2022-02-15 00:37:16 +0300

    .

--- server/src/utils/seeder.ts
+++ server/src/utils/seeder.ts
@@ -10,14 +10,29 @@ config();
 // This method is in no way part of the back end logic. This is to stimulate a on-a-second-basis data flow to the server, getting random 1000 players from the database and
 // sending it along with a random money value to be written to the data stream.
 
-export default async () => {
+export default () => {
   const { PORT } = process.env;
   const seederSocket: Socket = io(`http://127.0.0.1:${PORT}`);
-  const all: IPlayers = await Player.find().select("_id");
   setInterval(async () => {
-    const { _id } = all[Math.floor(Math.random() * all.length)];
+    const randomBunch: IPlayers = await Player.aggregate([
+      {
+        $sample: { size: 1000 },
+      },
+      {
+        $project: {
+          _id: 1,
+          userName: 0,
+          name: 0,
+          country: 0,
+          money: 0,
+          __v: 0,
+        },
+      },
+    ]);
 
-    const money: number = Math.floor(Math.random() * 1000);
-    seederSocket.emit(socketConstants.onDataFlowToRedisStream, [_id, money]);
-  }, 2);
+    randomBunch.forEach(({ _id }) => {
+      const money: number = Math.floor(Math.random() * 1000);
+      seederSocket.emit(socketConstants.onDataFlowToRedisStream, [_id, money]);
+    });
+  }, 1000);
 };
